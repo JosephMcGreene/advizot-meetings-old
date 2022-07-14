9f72bcbc6a7d1611ff7ce8eae24befda25de4e0e
Joseph McGreene <josephorianmcgreene@gmail.com>
2022-07-14 16:07:28 -0700

    Add /post route to take data from react form

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 // "start": "node src/server2/index.js",
 const express = require("express");
+const axios = require("axios");
 const app = express();
 
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+
+app.use(express.json());
+
 app.post("/post", (req, res) => {
-	console.log("Connected to React!");
-	res.redirect("/");
+	console.log(req.body);
+	console.log(req.body.name);
 });
 
 const PORT = process.env.PORT || 8080;
